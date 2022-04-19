d530b8ab2dbc831983cff17dc19c3f7a0aaafdcc
Kimberly Carmel Artienda <kim.artienda@gmail.com>
2022-04-20 00:32:03 +0800

    connected to heroku

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,8 @@ import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
 
-//when called, value of env will stored in 'process'
 dotenv.config();
-//to connect MONGODB
+
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
@@ -21,7 +20,6 @@ mongoose
 
 const app = express();
 
-//form data in post request will be converted to json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -31,17 +29,16 @@ app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
 const __dirname = path.resolve();
-//defining middleware
 app.use(express.static(path.join(__dirname, '/frontend/build')));
-//everything user enter in domain, will serve html file
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
